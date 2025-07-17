16c1a3f493a05ffb393c60a09788f2be953b703b
Martin Kunkel <ich@martinkunkel.de>
2025-07-17 17:24:39 +0200

    DO_NOT_MERGE: my local setup

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -eu
+cargo build
+cargo test timeout
+./util/build-gnu.sh
+
+./util/run-gnu-test.sh tests/timeout/timeout-blocked tests/timeout/timeout-group tests/timeout/timeout-large-parameters tests/timeout/timeout-parameters tests/timeout/timeout
